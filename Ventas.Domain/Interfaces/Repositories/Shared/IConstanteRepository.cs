6c75e52e2a93f4448b6c6665fdb1ab84bdf26be0
Dezbx <mr.david19xdd@gmail.com>
2026-01-05 23:11:32 -0500

    Se agrego el IsDeleted en la interfaz

--- Ventas.Domain/Interfaces/Repositories/Shared/IConstanteRepository.cs
+++ Ventas.Domain/Interfaces/Repositories/Shared/IConstanteRepository.cs
@@ -4,7 +4,7 @@ using Ventas.Domain.Interfaces.Repositories.Common;
 namespace Ventas.Domain.Interfaces.Repositories.Shared
 {
     public interface IConstanteRepository : IGenericRepository<Constante>
-    {
+    {   
         Task <IEnumerable<Constante>> ObtenerPorGrupoConstanteIdAsync(int grupoConstanteId);
         Task <bool> ExisteValorEnGrupoAsync(int grupoConstanteId, int valor);    
         Task  <bool> ExisteDescripcionEnGrupoAsync(int grupoConstanteId, string descripcion);
