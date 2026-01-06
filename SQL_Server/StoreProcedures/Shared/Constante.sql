3e0128433669301d7a07b78278e3d6f1de31ce5c
Dezbx <mr.david19xdd@gmail.com>
2026-01-05 23:08:24 -0500

    Se termino el sp para la tabla Constante

--- SQL_Server/StoreProcedures/Shared/Constante.sql
+++ SQL_Server/StoreProcedures/Shared/Constante.sql
@@ -13,9 +13,11 @@ AS
         C.GrupoConstanteId,
         C.Descripcion,
         C.Orden,
-        C.Valor
+        C.Valor,
+		C.IsDeleted
         FROM compartido.Constante AS C
             WHERE ConstanteId = @ConstanteId
+				AND C.IsDeleted = 0
     END
 GO
 
@@ -32,7 +34,8 @@ AS
         C.GrupoConstanteId,
         C.Descripcion,
         C.Orden,
-        C.Valor
+        C.Valor,
+		C.IsDeleted
         FROM compartido.Constante AS C
     END
 GO
@@ -42,13 +45,14 @@ CREATE OR ALTER PROCEDURE compartido.Constante_spAgregar
     @GrupoConstanteId	INT, 
     @Valor				INT,
     @Descripcion		VARCHAR(100),
-    @Orden				INT 
+    @Orden				INT,
+	@IsDeleted			BIT
 )
 AS
     BEGIN
         SET NOCOUNT ON;
-        INSERT INTO compartido.Constante (GrupoConstanteId, Valor, Descripcion, Orden)
-        VALUES (@GrupoConstanteId, @Valor, @Descripcion, @Orden)
+        INSERT INTO compartido.Constante (GrupoConstanteId, Valor, Descripcion, Orden, IsDeleted)
+        VALUES (@GrupoConstanteId, @Valor, @Descripcion, @Orden, @IsDeleted)
 
         -- Retorna el ID generado para que Dapper lo capture
         SELECT CAST(SCOPE_IDENTITY() AS INT);
@@ -62,7 +66,7 @@ CREATE OR ALTER PROCEDURE compartido.Constante_spActualizar
     @GrupoConstanteId	INT, 
     @Valor				INT,
     @Descripcion		VARCHAR(100),
-    @Orden				INT 
+    @Orden				INT
 )
 AS
     BEGIN
@@ -73,6 +77,7 @@ AS
             Descripcion = @Descripcion,
             Orden = @Orden
         WHERE ConstanteId = @ConstanteId
+			AND C.IsDeleted = 0;
     END
 GO
 
@@ -84,7 +89,13 @@ CREATE OR ALTER PROCEDURE compartido.Constante_spExistePorId
 AS
     BEGIN
         SET NOCOUNT ON;
-        IF EXISTS (SELECT 1 FROM compartido.Constante WHERE ConstanteId = @ConstanteId)
+        IF EXISTS 
+		(
+			SELECT 1 
+			FROM compartido.Constante 
+			WHERE ConstanteId = @ConstanteId  
+				AND C.IsDeleted = 0
+		)
             SELECT CAST(1 AS BIT);
         ELSE
             SELECT CAST(0 AS BIT)
@@ -99,118 +110,234 @@ AS
         SELECT 
         COUNT (C.ConstanteId)
         FROM compartido.Constante AS C
+		WHERE C.IsDeleted = 0
     END
 GO
 
 -- Necesario para la creacion de varios registros
 -- Creamos un tipo de tabla para el esquema compartido
+IF NOT EXISTS (SELECT * FROM sys.types WHERE name = 'ConstanteType' AND schema_id = SCHEMA_ID('compartido'))
+BEGIN
+    CREATE TYPE compartido.ConstanteType AS TABLE
+    (   
+			ConstanteId			INT NULL,
+			GrupoConstanteId	INT,
+			Valor				INT,
+			Descripcion			VARCHAR(100),
+			Orden				INT,
+			IsDeleted			BIT
+    );
+END
+GO
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spAgregarVarios
 (
-
+	@ConstanteLista compartido.ConstanteType READONLY	
 )
 AS
     BEGIN
         SET NOCOUNT ON;
-    END
-GO
+		INSERT INTO compartido.Constante (GrupoConstanteId, Valor, Descripcion, Orden,IsDeleted)
+		SELECT GrupoConstanteId, Valor, Descripcion, Orden, IsDeleted
+		FROM @ConstanteLista
 
+		-- Retorna la cantidad de registros insertados
+		SELECT @@ROWCOUNT;
+	END
+GO
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spActualizarVarios
 (
-
+	@ConstanteLista compartido.ConstanteType READONLY
 )
 AS
     BEGIN
         SET NOCOUNT ON;
+		UPDATE C 	-- Modificar tabla C (Constante) Linea 162
+		SET C.GrupoConstanteId = Lista.GrupoConstanteId,
+			C.Valor = Lista.Valor,
+			C.Descripcion = Lista.Descripcion,
+			C.Orden = Lista.Orden,
+			C.IsDeleted = Lista.IsDeleted
+		FROM compartido.Constante AS C 
+		INNER JOIN @ConstanteLista AS Lista
+			ON C.ConstanteId = Lista.ConstanteId
+		WHERE C.IsDeleted = 0
+
+		SELECT @@ROWCOUNT;
     END
 GO
 
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spDesactivarVarios
 (
-
+	@ConstanteLista compartido.ConstanteType READONLY
 )
 AS
     BEGIN
         SET NOCOUNT ON;
+
+		UPDATE C 	
+		SET C.IsDeleted = Lista.IsDeleted
+		FROM compartido.Constante AS C 
+		INNER JOIN @ConstanteLista AS Lista
+			ON C.ConstanteId = Lista.ConstanteId
+		WHERE C.IsDeleted = 0
+
+		SELECT @@ROWCOUNT;
+
     END
 GO
 
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spObtenerPorGrupoConstanteId
 (
-
+	@GrupoConstanteId INT
 )
 AS
     BEGIN
         SET NOCOUNT ON;
+		SELECT
+		C.ConstanteId,
+		C.GrupoConstanteId,
+		C.Valor,
+		C.Descripcion,
+		C.Orden,
+		C.IsDeleted
+		FROM compartido.Constante AS C
+			WHERE C.GrupoConstanteId = @GrupoConstanteId
+				AND C.IsDeleted = 0
     END
 GO
 
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spObtenerPorGrupoYValor
 (
-
+	@GrupoConstanteId INT,
+	@Valor INT
 )
 AS
     BEGIN
         SET NOCOUNT ON;
+		SELECT
+		C.ConstanteId,
+		C.GrupoConstanteId,
+		C.Valor,
+		C.Descripcion,
+		C.Orden,
+		C.IsDeleted
+		FROM compartido.Constante AS C
+			WHERE C.GrupoConstanteId = @GrupoConstanteId
+				AND C.Valor = @Valor
+				AND C.IsDeleted = 0
     END
 GO
 
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spExisteValorEnGrupo
 (
-
+	@GrupoConstanteId INT,
+	@Valor INT	
 )
 AS
     BEGIN
         SET NOCOUNT ON;
+		IF EXISTS 
+		(
+			SELECT 1 
+			FROM compartido.Constante  AS C
+			WHERE C.GrupoConstanteId = @GrupoConstanteId 
+				AND C.Valor = @Valor 
+		)
+			SELECT CAST (1 AS BIT) -- Devuelve 1 si existe
+		ELSE
+			SELECT CAST (0 AS BIT) -- Devuelve 0 si NO existe
     END
 GO
 
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spExisteDescripcionEnGrupo
 (
-
+	@GrupoConstanteId INT,
+	@Descripcion VARCHAR(100)
 )
 AS
     BEGIN
         SET NOCOUNT ON;
+		IF EXISTS 
+		(
+			SELECT 1 
+			FROM compartido.Constante  AS C
+			WHERE C.GrupoConstanteId = @GrupoConstanteId 
+				AND C.Descripcion = @Descripcion 
+		)
+			SELECT CAST (1 AS BIT) -- Devuelve 1 si existe
+		ELSE
+			SELECT CAST (0 AS BIT) -- Devuelve 0 si NO existe
     END
 GO
 
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spContarConstantesEnGrupo
 (
-
+	@GrupoConstanteId INT
 )
 AS
     BEGIN
         SET NOCOUNT ON;
+		SELECT 
+		COUNT (C.GrupoConstanteId) AS ConstantePorGrupo
+		FROM compartido.Constante  AS C
+			WHERE C.GrupoConstanteId = @GrupoConstanteId
     END
 GO
 
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spTieneConstantesAsociadas
 (
-
+	@ConstanteId INT
 )
 AS
     BEGIN
         SET NOCOUNT ON;
-    END
+		IF EXISTS 
+		(
+			SELECT 1 FROM core.Empleado WHERE EstadoConstanteId = @ConstanteId AND IsDeleted = 0
+			UNION ALL
+			-- Verificar en Clientes
+			SELECT 1 FROM core.Cliente WHERE EstadoConstanteId = @ConstanteId AND IsDeleted = 0
+			UNION ALL
+			-- Verificar en Proveedores
+			SELECT 1 FROM core.Proveedor WHERE EstadoConstanteId = @ConstanteId AND IsDeleted = 0
+			UNION ALL
+			-- Verificar en Ventas
+			SELECT 1 FROM core.Venta WHERE EstadoConstanteId = @ConstanteId AND IsDeleted = 0
+		)
+		    SELECT CAST(1 AS BIT); -- Devuelve True (1) si hay asociaciones
+		ELSE
+		    SELECT CAST(0 AS BIT); -- Devuelve False (0) si está libre
+		END
 GO
 
 
 CREATE OR ALTER PROCEDURE compartido.Constante_spObtenerConstantePorDescripcion
 (
-
+	@GrupoConstanteId INT,
+	@Descripcion VARCHAR(100)
 )
 AS
     BEGIN
         SET NOCOUNT ON;
+		SELECT 
+		C.ConstanteId,
+		C.GrupoConstanteId,
+		C.Valor,
+		C.Descripcion,
+		C.Orden,
+		C.IsDeleted
+		FROM compartido.Constante  AS C
+		WHERE C.GrupoConstanteId = @GrupoConstanteId 
+			AND C.Descripcion = @Descripcion
     END
 GO
 /*
