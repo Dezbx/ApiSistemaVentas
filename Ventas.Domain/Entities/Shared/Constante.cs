e98549608a17fa8e69dd7de1e1c214a903cdb005
Dezbx <mr.david19xdd@gmail.com>
2026-01-05 23:09:47 -0500

    Se agrego el IsDeleted como propiedad de la clase

--- Ventas.Domain/Entities/Shared/Constante.cs
+++ Ventas.Domain/Entities/Shared/Constante.cs
@@ -9,15 +9,17 @@ namespace Ventas.Domain.Entities.Shared
         public string Valor { get; set; } = string.Empty; //1,2,3,4,5, etc
         public DescripcionTexto Descripcion { get; private set; } = null!; // Pago en proceso, pago en ... etc
         public int Orden { get; set; } // Indica el orden en que se mostrará la constante dentro de su grupo
+        public bool IsDeleted { get; set; }
 
         protected Constante() { } // Para EF Core / Dapper
 
-        public Constante(int grupoConstanteId, string valor, DescripcionTexto descripcion, int orden)
+        public Constante(int grupoConstanteId, string valor, DescripcionTexto descripcion, int orden, bool isDeleted)
         {
             GrupoConstanteId = grupoConstanteId;
             Valor = valor;
             Descripcion = descripcion ?? throw new ArgumentNullException(nameof(descripcion));
             Orden = orden;
+            IsDeleted = isDeleted;
         }
 
         public void CambiarDescripcion(DescripcionTexto nuevaDescripcion)
